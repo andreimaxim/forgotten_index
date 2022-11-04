6723b59212706563636a24ec70e65a7e7ac2a1ad
Andrei Maxim <andrei@andreimaxim.ro>
2022-11-04 14:13:28 +0200

    Witness me.

---
+++ lib/forgotten_index.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "forgotten_index/version"
+
+module ForgottenIndex
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/forgotten_index/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ForgottenIndex
+  VERSION = "0.1.0"
+end
